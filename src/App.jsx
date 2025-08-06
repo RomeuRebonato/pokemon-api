ed645dd37369dd319e9180bf735eb91492eec5b4
RomeuRebonato <romeurebonato@gmail.com>
2025-08-06 09:50:57 -0300

    usequery react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,133 @@
-import { useState } from "react";
-import "./App.css";
+import { useRef } from "react";
 import PokemonData from "./PokemonData";
+import { useQuery } from "@tanstack/react-query";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 
-function App() {
-  const [name, setName] = useState(null);
-  const [data, setData] = useState(null);
-  const [SpeciesData, setSpeciesData] = useState(null);
-  const [error, setError] = useState(null);
-
-  function fetchPokemon(name) {
-    setError(null);
-    setData(null);
-    setSpeciesData(null);
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
-      .then((response1) => {
-        if (!response1.ok) {
-          throw new Error("Erro na busca do Pokémon");
-        }
-        return response1.json();
-      })
-      .then((data1) => {
-        setData(data1);
-        return fetch(
-          `https://pokeapi.co/api/v2/pokemon-species/${name.toLowerCase()}`
-        );
-      })
-      .then((response2) => {
+function fetchPokemon(name) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`)
+    .then((response1) => {
+      if (!response1.ok) throw new Error("Erro na busca do Pokémon");
+      return response1.json();
+    })
+    .then((data1) =>
+      fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${name.toLowerCase()}`
+      ).then((response2) => {
         if (!response2.ok)
           throw new Error("Erro na busca dos dados da espécie");
-        return response2.json();
+        return response2.json().then((data2) => ({
+          data: data1,
+          species: data2,
+        }));
       })
-      .then((data2) => {
-        setSpeciesData(data2);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  }
+    );
+}
+
+function Home() {
+  const inputRef = useRef();
+  const navigate = useNavigate();
+
+  const { data, error, isError, refetch } = useQuery({
+    queryKey: ["pokemon-search"],
+    queryFn: () => fetchPokemon(inputRef.current.value),
+    enabled: false,
+  });
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (name.trim()) {
-      fetchPokemon(name.trim());
+    refetch();
+  }
+
+  function handleClick() {
+    if (data?.data?.name) {
+      navigate(`/pokemon/${data.data.name}`);
     }
   }
 
   return (
-    <div>
-      <h1 style={{ fontFamily: "Arial, sans-serif" }}>Buscar Pokémon</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="bg-blue-100 text-[#131313] font-sans p-8 min-h-screen">
+      <h1 className="text-center text-gray-800 mb-4 text-4xl font-bold">
+        Buscar Pokémon
+      </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center flex-wrap gap-2 mb-8"
+      >
         <input
           type="text"
-          value={name || ""}
-          onChange={(e) => setName(e.target.value)}
+          ref={inputRef}
           placeholder="Digite o nome do Pokémon"
+          className="p-2 rounded-md border border-white w-full max-w-[240px]"
         />
-        <button type="submit">Buscar</button>
+        <button
+          type="submit"
+          className="p-2 px-10 bg-blue-600 hover:bg-blue-800 text-white font-bold rounded-md w-full max-w-[120px]"
+        >
+          Buscar
+        </button>
       </form>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {isError && <p className="text-red-600 mb-4">{error.message}</p>}
 
-      {data && SpeciesData && <PokemonData data={data} species={SpeciesData} />}
+      {data && (
+        <div className="mt-6 space-y-4 text-center">
+          <h2 className="text-2xl font-bold text-center uppercase text-gray-800">
+            {data.data.name.toUpperCase()} (ID: {data.id})
+          </h2>
+          <div className="flex justify-center">
+            <img
+              src={data.data.sprites.front_default}
+              alt="Imagem do Pokémon"
+              className="w-[300px] cursor-pointer hover:scale-105 transition-all duration-200"
+              onClick={handleClick}
+            />
+          </div>
+        </div>
+      )}
     </div>
+  );
+}
+
+function PaginaPokemon() {
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const { data, error, isError, isLoading } = useQuery({
+    queryKey: ["pokemon-details", name],
+    queryFn: () => fetchPokemon(name),
+  });
+
+  if (isLoading) return <p className="text-center">Carregando...</p>;
+  if (isError)
+    return <p className="text-center text-red-600">{error.message}</p>;
+
+  return (
+    <div className="p-8 bg-blue-100 text-[#131313] font-sans min-h-screen">
+      <button
+        className="mb-6 px-4 py-2 bg-blue-300 hover:bg-gray-400 rounded font-semibold"
+        onClick={() => navigate(-1)}
+      >
+        &#8656;
+      </button>
+      <PokemonData data={data.data} species={data.species} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:name" element={<PaginaPokemon />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/PokemonData.jsx
+++ src/PokemonData.jsx
@@ -1,67 +1,87 @@
 function PokemonData({ data, species }) {
   return (
-    <>
-      <h2>
+    <div className="bg-white rounded-lg shadow-lg max-w-2xl mx-auto p-6 space-y-6">
+      <h2 className="text-2xl font-bold text-center uppercase text-gray-800">
         {data.name.toUpperCase()} (ID: {data.id})
       </h2>
-      <img src={data.sprites.front_default} alt="imagem do pokemon" />
 
-      <p>
-        <strong>Tipo:</strong> {data.types.map((t) => t.type.name).join(", ")}
-      </p>
-      <p>
-        <strong>Altura:</strong> {data.height} cm
-      </p>
-      <p>
-        <strong>Peso:</strong> {data.weight / 10} kg
-      </p>
-      <p>
-        <strong>XP Base:</strong> {data.base_experience}
-      </p>
-      <p>
-        <strong>Habilidades:</strong>
-      </p>
-      <ul>
-        {data.abilities.map((a) => (
-          <li key={a.ability.name}>
-            {a.ability.name} (slot {a.slot}){a.is_hidden && " - Oculta"}
-          </li>
-        ))}
-      </ul>
+      <div className="flex justify-center">
+        <img
+          src={data.sprites.front_default}
+          alt="imagem do pokemon"
+          className="w-[300px] h-auto"
+        />
+      </div>
 
-      <h3>Estatísticas</h3>
-      <ul>
-        {data.stats.map((d) => (
-          <li key={d.stat.name}>
-            {d.stat.name}: {d.base_stat}
-          </li>
-        ))}
-      </ul>
+      <div className="space-y-2">
+        <p>
+          <strong>Tipo:</strong> {data.types.map((t) => t.type.name).join(", ")}
+        </p>
+        <p>
+          <strong>Altura:</strong> {data.height} cm
+        </p>
+        <p>
+          <strong>Peso:</strong> {data.weight / 10} kg
+        </p>
+        <p>
+          <strong>XP Base:</strong> {data.base_experience}
+        </p>
+      </div>
 
-      <h3>Índices de Jogo</h3>
-      <ul className="list-indices">
-        {data.game_indices.map((i, position) => (
-          <li className="line-indice" key={position}>
-            Versão: {i.version.name} - Índice: {i.game_index}
-          </li>
-        ))}
-      </ul>
+      <div className="space-y-2">
+        <p className="text-x1 font-semibold border-b pb-1">Habilidades:</p>
+        <ul className="space-y-1">
+          {data.abilities.map((a) => (
+            <li key={a.ability.name} className="bg-gray-100 rounded p-2">
+              {a.ability.name} (slot {a.slot}){a.is_hidden && " - Oculta"}
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <h3>Espécie</h3>
-      <p>
-        <strong>Cor:</strong> {species.color.name}
-      </p>
-      <p>
-        <strong>Felicidade Base:</strong> {species.base_happiness}
-      </p>
-      <p>
-        <strong>Taxa de Captura:</strong> {species.capture_rate}
-      </p>
-      <p>
-        <strong>Grupo de Ovos:</strong>{" "}
-        {species.egg_groups.map((g) => g.name).join(", ")}
-      </p>
-    </>
+      <div className="space-y-2">
+        <h3 className="text-x1 font-semibold border-b pb-1">Estatísticas</h3>
+        <ul className="space-y-1">
+          {data.stats.map((d) => (
+            <li key={d.stat.name} className="bg-gray-100 rounded p-2">
+              {d.stat.name}: {d.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="space-y-2">
+        <h3 className="text-x1 font-semibold border-b pb-1">Índices de Jogo</h3>
+        <ul className="grid grid-cols-[repeat(auto-fit,minmax(180px,1fr))] gap-4">
+          {data.game_indices.map((i, position) => (
+            <li
+              className="bg-gray-100 border border-gray-300 rounded-md p-3 shadow-sm"
+              key={position}
+            >
+              <strong> Versão:</strong> {i.version.name} <br />
+              <strong>Índice:</strong> {i.game_index}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="mb-2">
+        <h3 className="text-xl font-semibold mb-2">Espécie</h3>
+        <p>
+          <strong>Cor:</strong> {species.color.name}
+        </p>
+        <p>
+          <strong>Felicidade Base:</strong> {species.base_happiness}
+        </p>
+        <p>
+          <strong>Taxa de Captura:</strong> {species.capture_rate}
+        </p>
+        <p>
+          <strong>Grupo de Ovos:</strong>{" "}
+          {species.egg_groups.map((g) => g.name).join(", ")}
+        </p>
+      </div>
+    </div>
   );
 }
 
